1fedcbdfa8d51e129b884deadd5a72b575df475c
B1nstock <115542575+B1nstock@users.noreply.github.com>
2022-12-11 22:54:18 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let name= "Moon";
+console.log(name);
+let ageM= 19;
+console.log(ageM);
+
+let age7 = ageM*7;
+console.log(age7);
+
+let studentsL = ["Peepeepoopoo", "Salomé", "Josephina"];
+let numbers = [1, 2, 3, 4, 5];
+console.log(studentsL[0]);
+
+let students=[
+    {
+        name: "Peepeepoopoo"
+        age: 3500;
+        isStudent: true
+    },
+    {
+        name: "Salomé"
+        age: 150;
+        isStudent: true
+    },
+    {
+        name: "Josephina"
+        age: 420;
+        isStudent: false
+    }
+];
+
+console.log(students.name);
+//Peepeepoopoo
